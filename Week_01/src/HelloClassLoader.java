16e88405c80ea3b186e94277f8a55587860a517b
david.wang <mzh2008@163.com>
2021-01-16 19:50:41 +0800

    week01-方法2

--- Week_01/src/HelloClassLoader.java
+++ Week_01/src/HelloClassLoader.java
@@ -1,8 +1,7 @@
 import org.apache.commons.io.IOUtils;
 import sun.nio.ch.IOUtil;
 
-import java.io.File;
-import java.io.FileInputStream;
+import java.io.*;
 import java.lang.reflect.Method;
 
 public class HelloClassLoader extends ClassLoader {
@@ -17,14 +16,29 @@ public class HelloClassLoader extends ClassLoader {
 
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
-        byte[] bytes = null;
-        try (FileInputStream fileInputStream = new FileInputStream(new File(name))) {
-            bytes = IOUtils.toByteArray(fileInputStream);
-            for(int i = 0; i< bytes.length;i++){
-                bytes[i] = (byte) ((byte)255 - bytes[i]);
+//    1:
+//        byte[] bytes = null;
+//        try (FileInputStream fileInputStream = new FileInputStream(new File(name))) {
+//            bytes = IOUtils.toByteArray(fileInputStream);
+//            for(int i = 0; i< bytes.length;i++){
+//                bytes[i] = (byte) ((byte)255 - bytes[i]);
+//            }
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+
+        // 2:
+        byte[] bytes ;
+        try (FileInputStream fileInputStream = new FileInputStream(new File(name));
+             ByteArrayOutputStream bos = new ByteArrayOutputStream();) {
+            int bt;
+            while((bt = fileInputStream.read())!=-1){
+                bos.write((byte)255-(byte)bt);
             }
+            bytes = bos.toByteArray();
         }catch (Exception e){
             e.printStackTrace();
+            throw new RuntimeException(e);
         }
         return super.defineClass("Hello",bytes,0, bytes.length);
     }
