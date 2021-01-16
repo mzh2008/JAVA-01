8577e566974931b4055c55bf937f7450b27bc229
david.wang <mzh2008@163.com>
2021-01-16 19:40:53 +0800

    week01

--- Week_01/src/HelloClassLoader.java
+++ Week_01/src/HelloClassLoader.java
@@ -1,24 +1,27 @@
+import org.apache.commons.io.IOUtils;
+import sun.nio.ch.IOUtil;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.lang.reflect.Method;
 
 public class HelloClassLoader extends ClassLoader {
   public static void main(String[] args) throws Exception {
-//      Hello hello = new Hello();
-//      hello.hello();
+
 
       Class clazz  =  new HelloClassLoader().findClass("Hello.xlass");
       Method method = clazz.getMethod("hello");
       method.invoke(clazz.newInstance());
+
   }
 
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
-        byte[] bytes = new byte[2048];
+        byte[] bytes = null;
         try (FileInputStream fileInputStream = new FileInputStream(new File(name))) {
-            fileInputStream.read(bytes);
+            bytes = IOUtils.toByteArray(fileInputStream);
             for(int i = 0; i< bytes.length;i++){
-                bytes[i] = (byte) (255 - bytes[i]);
+                bytes[i] = (byte) ((byte)255 - bytes[i]);
             }
         }catch (Exception e){
             e.printStackTrace();
